2d37d931157a96ad11c46b2d59dd8c585bffa44c
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-02-03 00:36:56 +0900

    Reformatting code

--- src/pages/rooms/[roomId].tsx
+++ src/pages/rooms/[roomId].tsx
@@ -1,56 +1,60 @@
-import {NextComponentType, NextPage} from "next";
-import {useEffect, useRef} from "react";
-import {observer} from "mobx-react";
+import { NextComponentType, NextPage } from "next";
+import { useEffect, useRef } from "react";
+import { observer } from "mobx-react";
 import roomStore from "@/stores/RoomStore";
 
 const Room: NextPage = () => {
+  useEffect(() => {
+    roomStore.connectSocket();
+  }, []);
 
-    useEffect(() => {
-        roomStore.connectSocket()
-    }, [])
-
-    return (
-        <div>
-            <table className="mainTable">
-                <tbody>
-                <tr>
-                    <td className="localColumn">
-                        <LocalVideo/>
-                    </td>
-                    <td className="remoteColumn">
-                        <div id="remote-video-container"></div>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-            <table>
-                <tbody>
-                <tr>
-                    <td></td>
-                </tr>
-                </tbody>
-            </table>
-            <button id="video-toggle">Video OFF</button>
-            <button id="audio-toggle">Audio OFF</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <table className="mainTable">
+        <tbody>
+          <tr>
+            <td className="localColumn">
+              <LocalVideo />
+            </td>
+            <td className="remoteColumn">
+              <div id="remote-video-container"></div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <tbody>
+          <tr>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+      <button id="video-toggle">Video OFF</button>
+      <button id="audio-toggle">Audio OFF</button>
+    </div>
+  );
+};
 
 const LocalVideo: NextComponentType = observer(() => {
-    const localMediaStream = roomStore.localMediaStream
-    const localVideoRef = useRef<HTMLVideoElement | null>()
+  const localMediaStream = roomStore.localMediaStream;
+  const localVideoRef = useRef<HTMLVideoElement | null>();
 
-    useEffect(() => {
-        const localVideo = localVideoRef.current
-        if (localVideo && localMediaStream !== undefined) {
-            localVideo.srcObject = localMediaStream
-        }
-    }, [localMediaStream])
+  useEffect(() => {
+    const localVideo = localVideoRef.current;
+    if (localVideo && localMediaStream !== undefined) {
+      localVideo.srcObject = localMediaStream;
+    }
+  }, [localMediaStream]);
 
-    return (
-        <video ref={video => localVideoRef.current = video} id="localVideo" autoPlay className="video"
-               muted></video>
-    )
-})
+  return (
+    <video
+      ref={(video) => (localVideoRef.current = video)}
+      id="localVideo"
+      autoPlay
+      className="video"
+      muted
+    ></video>
+  );
+});
 
-export default Room
+export default Room;
--- src/stores/RoomStore.ts
+++ src/stores/RoomStore.ts
@@ -1,60 +1,60 @@
-import {RoomSocketService} from "@/service/RoomSocketService";
-import {makeAutoObservable} from "mobx";
+import { RoomSocketService } from "@/service/RoomSocketService";
+import { makeAutoObservable } from "mobx";
 
 const mediaConstraints = {
-    audio: true,
-    video: {
-        width: {
-            min: 640,
-            max: 1920
-        },
-        height: {
-            min: 400,
-            max: 1080
-        }
-    }
+  audio: true,
+  video: {
+    width: {
+      min: 640,
+      max: 1920,
+    },
+    height: {
+      min: 400,
+      max: 1080,
+    },
+  },
 };
 
 export enum RoomState {
-    CREATED,
-    CONNECTED,
-    JOINED,
+  CREATED,
+  CONNECTED,
+  JOINED,
 }
 
 export interface RoomObserver {
-    onConnected: () => void;
-    onJoined: () => void;
+  onConnected: () => void;
+  onJoined: () => void;
 }
 
 class RoomStore implements RoomObserver {
+  private readonly _roomService = new RoomSocketService(this);
 
-    private readonly _roomService = new RoomSocketService(this)
+  private _localMediaStream?: MediaStream = undefined;
 
-    private _localMediaStream?: MediaStream = undefined
+  constructor() {
+    makeAutoObservable(this);
+  }
 
-    constructor() {
-        makeAutoObservable(this)
-    }
+  public get localMediaStream(): MediaStream | undefined {
+    return this._localMediaStream;
+  }
 
-    public get localMediaStream(): MediaStream | undefined {
-        return this._localMediaStream
-    }
+  public connectSocket = () => {
+    this._roomService.connect();
+  };
 
-    public connectSocket = () => {
-        this._roomService.connect()
-    }
+  private getLocalMedia = async () => {
+    this._localMediaStream = await navigator.mediaDevices.getUserMedia(
+      mediaConstraints
+    );
+  };
 
-    private getLocalMedia = async () => {
-        this._localMediaStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
-    }
+  public onConnected(): void {
+    this.getLocalMedia().then();
+  }
 
-    public onConnected(): void {
-        this.getLocalMedia().then()
-    }
-
-    public onJoined(): void {
-    }
+  public onJoined(): void {}
 }
 
-const roomStore = new RoomStore()
-export default roomStore
+const roomStore = new RoomStore();
+export default roomStore;
