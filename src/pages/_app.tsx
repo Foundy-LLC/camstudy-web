060e7a93b129f3b068fcc13dc41a0f223cb9559a
DaeHyun Kwon <104804antonio@gmail.com>
2023-02-13 17:59:20 +0900

    로그인 하지 않은 유저 리다이렉트 처리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+import ProtectedRoutes from "@/components/ProtectedRoutes";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <ProtectedRoutes router={router}>
+      <Component {...pageProps} />
+    </ProtectedRoutes>
+  );
 }
