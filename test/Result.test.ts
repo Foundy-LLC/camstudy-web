7aa31b738d75058dba030bd5378effa6e204f15d
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-02-13 10:29:36 +0900

    Result 클래스 리팩토링 (#27)

---
+++ test/Result.test.ts
@@ -0,0 +1,18 @@
+import { Result } from "@/models/common/Result";
+
+describe("Result", () => {
+  it("success", () => {
+    const result = Result.success(10);
+
+    expect(result.getOrNull()).toBe(10);
+    expect(result.throwableOrNull()).toBeUndefined();
+  });
+
+  it("error", () => {
+    const error = Error("ERROR");
+    const result = Result.error(error);
+
+    expect(result.getOrNull()).toBeUndefined();
+    expect(result.throwableOrNull()).toBe(error);
+  });
+});
