8c5784fc1ee2084399be5fffd0cb69d4c1afac97
kgunwoo26 <56541313+kgunwoo26@users.noreply.github.com>
2023-03-08 15:29:06 +0900

    Update src/constants/FriendMessage.ts
    Co-authored-by: Minseong Kim <57604817+jja08111@users.noreply.github.com>

--- src/constants/FriendMessage.ts
+++ src/constants/FriendMessage.ts
@@ -5,7 +5,7 @@ export const FRIEND_REQUEST_DUPLICATED_ERROR =
   "이미 친구 요청을 보낸 회원입니다.";
 export const FRIEND_REQUEST_CANCEL_SUCCESS =
   "친구 요청을 성공적으로 취소했습니다.";
-export const FRIEND_CANCEL_REQUEST_ERROR =
+export const NOT_FOUND_FRIEND_REQUEST_ERROR =
   "요청이 존재하지 않아 친구 요청을 취소할 수 없습니다.";
 export const FRIEND_REQUEST_ID_ERROR = "본인에게 친구 요청을 보낼 수 없습니다.";
 export const FRIEND_CANCEL_REQUEST_ID_ERROR =
