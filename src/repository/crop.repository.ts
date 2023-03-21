5cee62568c485461d35de901eedae2cd68fb7ba1
kgunwoo26 <khaerim41@naver.com>
2023-03-21 23:57:46 +0900

    수확한 작물 조회 구현

--- src/repository/crop.repository.ts
+++ src/repository/crop.repository.ts
@@ -2,6 +2,27 @@ import { uuidv4 } from "@firebase/util";
 import { CropCreateRequestBody } from "@/models/crop/CropCreateRequestBody";
 import prisma from "../../prisma/client";
 import { CropDeleteRequestBody } from "@/models/crop/CropDeleteRequestBody";
+import { Crop } from "@/models/crop/Crop";
+
+export const getHarvestedCrops = async (userId: string): Promise<Crop[]> => {
+  const harvestedCrops = await prisma.crops.findMany({
+    where: {
+      user_id: userId,
+      harvested_at: {
+        not: null,
+      },
+    },
+    orderBy: {
+      harvested_at: "desc",
+    },
+  });
+  return harvestedCrops.map((crop) => {
+    return {
+      type: crop.type,
+      grade: crop.grade!,
+    };
+  });
+};
 
 export const isExistGrowingCrop = async (userId: string) => {
   const growingCrops = await prisma.crops.findMany({
---
+++ src/stores/CropStore.tsx
@@ -0,0 +1,44 @@
+import { RootStore } from "@/stores/RootStore";
+import { Crop } from "@/models/crop/Crop";
+import { CropService, cropService } from "@/service/crop.service";
+import userStore from "@/stores/UserStore";
+import { NO_USER_STORE_ERROR_MESSAGE } from "@/constants/message";
+import { makeAutoObservable, runInAction } from "mobx";
+
+export class CropStore {
+  readonly rootStore: RootStore;
+  private _harvestedCrops: Crop[] = [];
+  constructor(
+    root: RootStore,
+    private readonly _cropService: CropService = cropService
+  ) {
+    this.rootStore = root;
+    makeAutoObservable(this);
+  }
+  public get harvestedCrops() {
+    return this._harvestedCrops;
+  }
+
+  public getHarvestedCrops = async () => {
+    try {
+      if (!userStore.currentUser) {
+        throw new Error(NO_USER_STORE_ERROR_MESSAGE);
+      }
+      const uid = userStore.currentUser.id;
+      const result = await this._cropService.getHarvestedCrops(uid);
+      if (result.isSuccess) {
+        runInAction(() => {
+          this._harvestedCrops = result.getOrNull()!;
+        });
+      } else {
+        runInAction(() => {
+          throw new Error(result.throwableOrNull()!.message);
+        });
+      }
+    } catch (e) {
+      runInAction(() => {
+        if (e instanceof Error) console.error(e.message);
+      });
+    }
+  };
+}
