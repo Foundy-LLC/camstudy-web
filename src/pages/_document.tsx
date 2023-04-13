37b698638ee9a4c61f713c88ae5976241cdda7da
kgunwoo26 <khaerim41@naver.com>
2023-04-14 02:15:38 +0900

    닉네임 및 자기소개 ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,10 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
       </Head>
       <body>
         <Main />
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,4 @@
-import {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-  NextPage,
-} from "next";
-import { verifyUserToken } from "@/service/verifyUserToken";
+import { NextPage } from "next";
 import useSWR from "swr";
 import { User } from "@/models/user/User";
 import React from "react";
@@ -25,13 +20,11 @@ const UserProfile: NextPage = observer(() => {
   const [user, loading] = useAuthState(auth);
   const { roomListStore } = useStores();
   const fetcher = (args: string) => fetch(args).then((res) => res.json());
-  const { data, error, isLoading } = useSWR<ResponseBody<User>>(
+  const { data, isLoading } = useSWR<ResponseBody<User>>(
     `/api/users/${user?.uid}`,
     fetcher
   );
   const profile = data?.data;
-
-  if (error) return "error";
 
   if (loading) {
     return <div>Loading</div>;
@@ -43,116 +36,174 @@ const UserProfile: NextPage = observer(() => {
   }
 
   if (data?.message === NOT_FOUND_USER_MESSAGE || profile === undefined)
-    return NOT_FOUND_USER_MESSAGE;
-  if (isLoading) return "Loading";
-  if (profile != undefined)
     return (
       <>
-        <section className={"box"}>
-          <div className={"box-header-margin"}>
-            <Header userId={user.uid} />
-          </div>
-          <div className={"box-contents-margin"}>
-            <div className={"box-contents"}>
-              <div className={"box-contents-side-menu"}>
-                <SideMenuBar userId={user.uid}></SideMenuBar>
-              </div>
-              <div className={"box-contents-item"}>
-                <div
-                  className={`${profileStyles["profile-image-form"]} elevation__card__search-bar__contained-button--waiting__etc`}
-                >
-                  <div className={`${profileStyles["title"]}`}>
-                    <span className="material-symbols-outlined">image</span>
-
-                    <label className={"typography__text--big"}>
-                      프로필 사진
-                    </label>
+        <h3>{NOT_FOUND_USER_MESSAGE}</h3>
+      </>
+    );
+  if (isLoading)
+    return (
+      <>
+        <h3>loading</h3>
+      </>
+    );
+  return (
+    <>
+      <section className={"box"}>
+        <div className={"box-header-margin"}>
+          <Header userId={user.uid} />
+        </div>
+        <div className={"box-contents-margin"}>
+          <div className={"box-contents"}>
+            <div className={"box-contents-side-menu"}>
+              <SideMenuBar userId={user.uid}></SideMenuBar>
+            </div>
+            <div className={"box-contents-item"}>
+              <div>
+                <div className={`${profileStyles["page-title"]}`}>
+                  <label
+                    className={`${profileStyles["title"]} typography__sub-headline`}
+                  >
+                    내 프로필
+                  </label>
+                  <div>
+                    <label>프로필 변경사항 저장하기</label>
                   </div>
-                  <div className={`${profileStyles["image-upload"]}`}>
-                    {roomListStore.imageUrl === "" ? (
-                      <div className={`${profileStyles["image"]}`}></div>
-                    ) : (
-                      <Image
-                        className={`${profileStyles["image"]}`}
-                        alt={"selected-img"}
-                        src={roomListStore.imageUrl}
-                        width={152}
-                        height={152}
-                      />
-                    )}
-                    <div className={`${profileStyles["precautions"]}`}>
-                      <label className={"typography__text--small"}>
-                        이런 프로필 사진은 안돼요
-                      </label>
-                      <ul className={"typography__caption"}>
-                        <li>
-                          &nbsp;· &nbsp; 타인에게 불쾌감을 줄 수 있는 사진
-                        </li>
-                        <li>&nbsp;· &nbsp; 5mb을 초과하는 용량의 이미지</li>
-                        <li>
-                          &nbsp;· &nbsp; 자기 자신이 아닌 타인의 사진 도용
-                        </li>
-                        <li>&nbsp;· &nbsp; 그 외 부적절하다고 판단되는 사진</li>
-                      </ul>
-                      <div
-                        className={`${profileStyles["image-upload-button"]}`}
-                      >
-                        <input
-                          id="roomThumbnail"
-                          type="file"
-                          accept="image/png, image/jpeg"
-                          onChange={(e) => {
-                            if (e.target.files) {
-                              roomListStore.importRoomThumbnail(
-                                e.target.files[0]
-                              );
-                            }
-                          }}
-                          hidden
-                        />
-                        <div
-                          onClick={() => {
-                            document.getElementById("roomThumbnail")!.click();
-                          }}
-                        >
-                          <span className="material-symbols-outlined">
-                            image
-                          </span>
-                          <label className={"typography__text"}>
-                            사진 업로드하기
+                  <div>
+                    <label>되돌리기</label>
+                  </div>
+                </div>
+                <div>
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div
+                      className={`${profileStyles["profile-image-form"]} elevation__card__search-bar__contained-button--waiting__etc`}
+                    >
+                      <div className={`${profileStyles["title"]}`}>
+                        <span className="material-symbols-sharp">image</span>
+                        <label className={"typography__text--big"}>
+                          프로필 사진
+                        </label>
+                      </div>
+                      <div className={`${profileStyles["image-upload"]}`}>
+                        {roomListStore.imageUrl === "" ? (
+                          <div className={`${profileStyles["image"]}`}></div>
+                        ) : (
+                          <Image
+                            className={`${profileStyles["image"]}`}
+                            alt={"selected-img"}
+                            src={roomListStore.imageUrl}
+                            width={152}
+                            height={152}
+                          />
+                        )}
+                        <div className={`${profileStyles["precautions"]}`}>
+                          <label className={"typography__text--small"}>
+                            이런 프로필 사진은 안돼요
                           </label>
+                          <ul className={"typography__caption"}>
+                            <li>
+                              &nbsp;· &nbsp; 타인에게 불쾌감을 줄 수 있는 사진
+                            </li>
+                            <li>&nbsp;· &nbsp; 5mb을 초과하는 용량의 이미지</li>
+                            <li>
+                              &nbsp;· &nbsp; 자기 자신이 아닌 타인의 사진 도용
+                            </li>
+                            <li>
+                              &nbsp;· &nbsp; 그 외 부적절하다고 판단되는 사진
+                            </li>
+                          </ul>
+                          <div
+                            className={`${profileStyles["image-upload-button"]}`}
+                          >
+                            <input
+                              id="roomThumbnail"
+                              type="file"
+                              accept="image/png, image/jpeg"
+                              onChange={(e) => {
+                                if (e.target.files) {
+                                  roomListStore.importRoomThumbnail(
+                                    e.target.files[0]
+                                  );
+                                }
+                              }}
+                              hidden
+                            />
+                            <div
+                              onClick={() => {
+                                document
+                                  .getElementById("roomThumbnail")!
+                                  .click();
+                              }}
+                            >
+                              <span className="material-symbols-sharp">
+                                image
+                              </span>
+                              <label className={"typography__text"}>
+                                사진 업로드하기
+                              </label>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
+                  <div
+                    className={`${profileStyles["nickname-form"]} elevation__card__search-bar__contained-button--waiting__etc`}
+                  >
+                    <div className={`${profileStyles["title"]}`}>
+                      <span className="material-symbols-sharp">
+                        text_snippet
+                      </span>
+                      <label className={"typography__text--big"}>
+                        닉네임 및 자기소개
+                      </label>
+                    </div>
+                    <div className={`${profileStyles["nickname"]} `}>
+                      <label className={"typography__caption"}>닉네임</label>
+                      <input
+                        type={"text"}
+                        className={"typography__text--small"}
+                      />
+                      <label className={"typography__caption"}>
+                        다른 농부들에게 표시되는 닉네임입니다
+                      </label>
+                    </div>
+                    <div className={`${profileStyles["introduce"]}`}>
+                      <label className={"typography__caption"}>자기소개</label>
+                      <input
+                        type={"text"}
+                        className={"typography__text--small"}
+                      />
+                      <label className={"typography__caption"}>
+                        나를 나타낼 수 있는 소개 내용을 입력해주세요
+                      </label>
+                    </div>
+                  </div>
                 </div>
-                <div>
-                  <UserProfileImage
-                    userId={user.uid}
-                    width={150}
-                    height={150}
-                  />
-                  <h1>아이디: {profile.id}</h1>
-                  <h1>이름: {profile.name}</h1>
-                  <h1>태그: {profile.tags}</h1>
-                  <h1>소개: {profile.introduce}</h1>
-                  <h1>총 공부 시간(분): {profile.totalStudyMinute}</h1>
-                  <h1>소속: {profile.organizations}</h1>
-                  <h1>랭킹: {profile.rankingScore}</h1>
-                </div>
+              </div>
+              <div>
+                <UserProfileImage userId={user.uid} width={150} height={150} />
+                <h1>아이디: {profile.id}</h1>
+                <h1>이름: {profile.name}</h1>
+                <h1>태그: {profile.tags}</h1>
+                <h1>소개: {profile.introduce}</h1>
+                <h1>총 공부 시간(분): {profile.totalStudyMinute}</h1>
+                <h1>소속: {profile.organizations}</h1>
+                <h1>랭킹: {profile.rankingScore}</h1>
               </div>
             </div>
           </div>
-        </section>
-        <style jsx>
-          {`
-            .material-symbols-outlined {
-              font-variation-settings: "FILL" 1, "wght" 500, "GRAD" 0, "opsz" 48;
-            }
-          `}
-        </style>
-      </>
-    );
+        </div>
+      </section>
+      <style jsx>
+        {`
+          .material-symbols-sharp {
+            font-variation-settings: "FILL" 1, "wght" 400, "GRAD" 0, "opsz" 48;
+          }
+        `}
+      </style>
+    </>
+  );
 });
 
 export default UserProfile;
