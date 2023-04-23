8c1b9308c75015758c23ca058ca9eb845730c1b5
DaeHyun Kwon <104804antonio@gmail.com>
2023-04-23 17:12:17 +0900

    대시보드 UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
 import { observer } from "mobx-react";
 import React, { useEffect, useState } from "react";
-import {
-  IMAGE_SERVER_URL,
-  USER_DEFAULT_IMAGE_SRC,
-} from "@/constants/image.constant";
+import { IMAGE_SERVER_URL } from "@/constants/image.constant";
 import {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
@@ -17,6 +13,8 @@ import { useStores } from "@/stores/context";
 import { HarvestedCrop } from "@/models/crop/HarvestedCrop";
 import { Layout } from "@/components/Layout";
 import { crops_type } from "@prisma/client";
+import { RoomItemGroup } from "@/pages/rooms";
+import dashboardStyles from "@/styles/dashboard.module.scss";
 
 // TODO 페이지 들어갈 때 유저 쿠키가 유효한지 판단함. 중복되는 코드라서 따로 빼보는 방법 찾아 볼 것.
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
@@ -48,9 +46,6 @@ function Home(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const userProfileImageLoader = ({ src }: { src: string }): string => {
     return `${IMAGE_SERVER_URL}/users/${src}.png`;
   };
-  const loggingUserInformation = () => {
-    console.log(JSON.stringify(userStore.currentUser));
-  };
 
   // 테스트용
   const setCrop = async () => {
@@ -65,6 +60,13 @@ function Home(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
     });
   };
 
+  const { roomListStore } = useStores();
+
+  useEffect(() => {
+    roomListStore.setMasterId(src);
+    roomListStore.fetchRooms();
+  });
+
   useEffect(() => {
     setErrorMessage(
       userStore.errorMessage == undefined ? "" : userStore.errorMessage
@@ -73,46 +75,83 @@ function Home(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
   return (
     <Layout>
-      <div>
-        <Image
-          width={150}
-          height={150}
-          loader={userProfileImageLoader}
-          src={src}
-          alt={"user profile image"}
-          onError={(e) => {
-            e.currentTarget.onerror = null;
-            setSrc(USER_DEFAULT_IMAGE_SRC);
-          }}
-        ></Image>
-        <button onClick={() => loggingUserInformation()}>
-          getUserInformation
-        </button>
-        <button onClick={() => router.push(`/users/${props.uid}`)}>
-          내 프로필
-        </button>
-        <button onClick={() => setCrop()}>작물</button>
-        <button
-          onClick={() => {
-            cropStore.getHarvestedCrops();
-          }}
-        >
-          인벤토리
-        </button>
-        <button
-          onClick={() =>
-            userStore.signOut().then(() => {
-              router.push("/login");
-            })
-          }
-        >
-          sign out
-        </button>
-        <HarvestedCropGroup items={cropStore.harvestedCrops} />
-        <div>{errorMessage}</div>
+      <div className={"typography__sub-headline"} style={{ padding: "20px" }}>
+        대시보드
       </div>
+      <Dashboard />
+      <RoomItemGroup items={roomListStore.roomOverviews} />
+      {roomListStore.errorMessage === undefined ? null : (
+        <h3>{roomListStore.errorMessage}</h3>
+      )}
     </Layout>
   );
 }
+
+export const Dashboard: NextPage = () => {
+  return (
+    <div className={"dashboard"}>
+      <div className={`${dashboardStyles["dashboard-frame"]}`}>
+        <div className={`${dashboardStyles["inner-frame"]}`}>
+          <div className={`${dashboardStyles["title"]}`}>
+            <span className="material-symbols-outlined">schedule</span>
+            <span>주간 공부시간</span>
+          </div>
+          <div className={`${dashboardStyles["content"]}`}>
+            <div
+              className={"typography__title1--big time"}
+              style={{ color: "#DDA30E" }}
+            >
+              56:38:17
+            </div>
+            <div className={"typography__text"} style={{ color: "#646464" }}>
+              전체 유저 기준 상위 6%
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${dashboardStyles["dashboard-frame"]}`}>
+        <div className={`${dashboardStyles["inner-frame"]}`}>
+          <div className={`${dashboardStyles["title"]}`}>
+            <span className="material-symbols-outlined">insert_chart</span>
+            <span>이번 주 랭킹</span>
+          </div>
+          <div className={`${dashboardStyles["content"]}`}>
+            <div
+              className={"typography__title1--big time"}
+              style={{ color: "#DDA30E" }}
+            >
+              7위
+            </div>
+            <div className={"typography__text"} style={{ color: "#646464" }}>
+              전체 유저 기준 862등
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${dashboardStyles["dashboard-frame"]}`}>
+        <div className={`${dashboardStyles["inner-frame"]}`}>
+          <div className={`${dashboardStyles["title"]}`}>
+            <span className="material-symbols-outlined">eco</span>
+            <span>내 작물</span>
+          </div>
+          <div className={`${dashboardStyles["content"]}`}></div>
+        </div>
+      </div>
+      <style jsx>{`
+        .dashboard {
+          display: flex;
+          gap: 20px;
+          margin: auto;
+          margin-bottom: 24px;
+        }
+        .material-symbols-outlined {
+          font-variation-settings: "FILL" 1, "wght" 400, "GRAD" 0, "opsz" 48;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default observer(Home);
