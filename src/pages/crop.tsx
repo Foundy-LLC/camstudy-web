c174ca7ed5935ac5573172db702280e81cd2c2f3
DaeHyun Kwon <104804antonio@gmail.com>
2023-05-03 15:19:06 +0900

    Update crop.tsx

--- src/pages/crop.tsx
+++ src/pages/crop.tsx
@@ -1,7 +1,40 @@
-import { NextPage } from "next";
-
-const crop: NextPage = () => {
-  return <div>asd</div>;
-};
-
-export default crop;
+// import { NextPage } from "next";
+// import { Layout } from "@/components/Layout";
+// import Image from "next/image";
+// import { useStores } from "@/stores/context";
+// import { useAuth } from "@/components/AuthProvider";
+// import React from "react";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { auth } from "@/service/firebase";
+// import router from "next/router";
+//
+// const crop: NextPage = () => {
+//   return <Layout>asd</Layout>;
+// };
+//
+// const myPot: NextPage = () => {
+//   const { cropStore } = useStores();
+//   const [user, loading] = useAuthState(auth);
+//
+//   if (loading) {
+//     return <div>Loading</div>;
+//   }
+//   if (!user) {
+//     router.replace("/login");
+//     return <div>Please sign in to continue</div>;
+//   }
+//
+//   return (
+//     <div>
+//       <div>내 화분</div>
+//       <div>
+//         <div>
+//           <Image src={} alt={}></Image>
+//         </div>
+//         <div></div>
+//       </div>
+//     </div>
+//   );
+// };
+//
+// export default crop;
