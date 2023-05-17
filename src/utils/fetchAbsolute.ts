72af2edc037513eab3f92dfb0490bdff775f96d8
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-05-17 21:43:42 +0900

    Client fetch할때 BASE_URL만 이용하도록 수정 (#145)

--- src/utils/fetchAbsolute.ts
+++ src/utils/fetchAbsolute.ts
@@ -1,9 +1,9 @@
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-const PORT_NUM = process.env.NEXT_PUBLIC_PORT_NUM;
+
 export const fetchAbsolute = async (
   path: RequestInfo | URL,
   init: RequestInit
 ): Promise<Response> => {
-  const url = `${BASE_URL}:${PORT_NUM}/${path}`;
+  const url = `${BASE_URL}/${path}`;
   return await fetch(url, init);
 };
