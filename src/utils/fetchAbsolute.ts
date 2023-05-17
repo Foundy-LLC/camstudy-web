d340eb5adf2713e35d487a035819d7738eeed47d
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-05-17 22:04:53 +0900

    Client API fetch 함수들 리팩토링 및 next.config의 remotePattern 수정 (#146)

--- src/utils/fetchAbsolute.ts
+++ src/utils/fetchAbsolute.ts
@@ -7,3 +7,19 @@ export const fetchAbsolute = async (
   const url = `${BASE_URL}/${path}`;
   return await fetch(url, init);
 };
+
+export const mediaRouterApiFetch = async (
+  path: RequestInfo | URL,
+  init: RequestInit
+): Promise<Response> => {
+  const url = `${BASE_URL}/media-router/${path}`;
+  return await fetch(url, init);
+};
+
+export const rankingApiFetch = async (
+  path: RequestInfo | URL,
+  init: RequestInit
+): Promise<Response> => {
+  const url = `${BASE_URL}/rank-server/${path}`;
+  return await fetch(url, init);
+};
